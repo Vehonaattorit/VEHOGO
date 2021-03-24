f8922a6f25d8d617fde7916824ca411cacd0cc49
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2021-03-24 15:30:49 +0200

    mainpage created and logout button added there

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import React, {useState, useEffect} from 'react'
 import {View} from 'react-native'
 import MainStackNavigator from './navigators/Navigator'
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font';
 
 export default function App() {
+
+  const [fontReady, setFontReady] = useState(false);
+
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    setFontReady(true);
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontReady) {
+    console.log('Waiting for fonts...');
+    return (
+      <AppLoading/>
+    );
+  }
   return (
     <View style={{flex: 1}}>
       <MainStackNavigator />
