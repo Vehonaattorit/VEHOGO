ca47accdf197b349d3bb59cd1c334167e673c68c
Michael Lock <thelockymichael@gmail.com>
2021-03-24 18:28:24 +0200

    chatroom styling and mockup data

--- App.js
+++ App.js
@@ -2,35 +2,31 @@ import React, {useState, useEffect} from 'react'
 import {View} from 'react-native'
 import MainStackNavigator from './navigators/Navigator'
 import AppLoading from 'expo-app-loading'
-import * as Font from 'expo-font';
+import * as Font from 'expo-font'
+import ChatRoom from './views/ChatRoom'
 
 export default function App() {
-
-  const [fontReady, setFontReady] = useState(false);
-
+  const [fontReady, setFontReady] = useState(false)
 
   const loadFonts = async () => {
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-    });
-    setFontReady(true);
-  };
+    })
+    setFontReady(true)
+  }
 
   useEffect(() => {
-    loadFonts();
-  }, []);
+    loadFonts()
+  }, [])
 
   if (!fontReady) {
-    console.log('Waiting for fonts...');
-    return (
-      <AppLoading/>
-    );
+    console.log('Waiting for fonts...')
+    return <AppLoading />
   }
   return (
     <View style={{flex: 1}}>
-    <MainStackNavigator />
-
+      <ChatRoom />
     </View>
   )
 }
---
+++ views/ChatRoom.js
@@ -0,0 +1,152 @@
+import React, {useState, useEffect, useContext} from 'react'
+
+// import {IconButton} from "react-native-paper"
+
+import {GiftedChat, Bubble, Send, SystemMessage} from 'react-native-gifted-chat'
+import {IconButton} from 'react-native-paper'
+import {StyleSheet, View, Text, ActivityIndicator} from 'react-native'
+
+export default ChatRoom = ({route}) => {
+  const [currentUser, setCurrentUser] = useState(5)
+
+  const [messages, setMessages] = useState([
+    /**
+     * Mock message data
+     *
+     */
+
+    // example of system message
+    {
+      _id: 0,
+      text: 'New room created.',
+      createdAt: new Date().getTime(),
+      system: true,
+    },
+    {
+      _id: 1,
+      text: 'Hello !',
+      createdAt: new Date().getTime(),
+      user: {
+        _id: 2,
+        name: 'User Test',
+      },
+    },
+  ])
+
+  const renderSystemMessage = (props) => {
+    return (
+      <SystemMessage
+        {...props}
+        wrapperStyle={styles.systemMessageWrapper}
+        textStyle={styles.systemMessageText}
+      />
+    )
+  }
+
+  const handleSend = async (messages) => {
+    const text = messages[0].text
+    console.log('text', text)
+
+    const messageObject = {
+      _id: messages.length + 1,
+      text,
+      createdAt: new Date().getTime(),
+      user: {
+        _id: currentUser,
+        name: 'Pekka Puupää',
+      },
+    }
+
+    setMessages(messages.concat(messageObject))
+  }
+
+  const renderLoading = () => {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#6646ee" />
+      </View>
+    )
+  }
+
+  const scrollToBottomComponent = () => {
+    return (
+      <View style={styles.bottomComponentContainer}>
+        <IconButton icon="chevron-double-down" size={36} color="#6646ee" />
+      </View>
+    )
+  }
+
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+          <IconButton icon="send-circle" size={36} color="#6646ee" />
+        </View>
+      </Send>
+    )
+  }
+
+  const renderBubble = (props) => {
+    return (
+      // Step 3: return the component
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            // Here is the color change
+            backgroundColor: '#6646ee',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#fff',
+          },
+        }}
+      />
+    )
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={handleSend}
+      user={{_id: currentUser.uid}}
+      renderBubble={renderBubble}
+      placeholder="Type your message here..."
+      showUserAvatar
+      alwaysShowSend
+      renderSend={renderSend}
+      scrollToBottom
+      scrollToBottomComponent={scrollToBottomComponent}
+      renderLoading={renderLoading}
+      renderSystemMessage={renderSystemMessage}
+    />
+  )
+}
+
+// Add corresponding styles
+const styles = StyleSheet.create({
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomComponentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  systemMessageWrapper: {
+    backgroundColor: '#6646ee',
+    borderRadius: 4,
+    padding: 5,
+  },
+  systemMessageText: {
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+})
