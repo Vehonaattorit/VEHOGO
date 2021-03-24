0b9bbd20913278f002f61434a4adfac45bf4c910
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-03-24 15:06:59 +0200

    WorkingDays.js modified

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 16,
+    padding: 10,
     textTransform: 'uppercase',
     color: Platform.OS == 'ios' ? color.sText : color.pText,
   },
--- views/WorkingHours.js
+++ views/WorkingHours.js
@@ -1,68 +1,23 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, Button, View} from 'react-native';
-import {color} from '../constants/colors';
+import React, {useState} from 'react'
+import {StyleSheet,Platform, Text, Button, View} from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker';
+import {color} from '../constants/colors'
+
+import {CustomButton} from '../components/CustomButton'
 export const WorkingHours = ({navigation}) => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-  };
-
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
-  const showTimepicker2 = () => {
-    showMode('time');
-  };
+  const [date, setDate] = useState(new Date())
 
   return (
     <View style={styles.container}>
-      <Text>Open up WorkingHours.js to start working on your app!</Text>
-      <View>
-        <Button onPress={showTimepicker} title="Show date picker!" />
-      </View>
-      <View>
-        <Button onPress={showTimepicker2} title="Show time picker!" />
-      </View>
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
-        />
-      )}
-
-      <Button
-        title="Submit"
-        onPress={() => {
-          navigation.navigate('setUpInit');
-        }}
-      />
+      <CustomButton title="Submit" />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: color.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  signUpContainer: {
-    margin: 5,
-    backgroundColor: 'yellow',
-  },
-});
+})
