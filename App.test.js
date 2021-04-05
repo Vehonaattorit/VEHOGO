dea3730da898aa28c63bc8635f83bf1aeebae054
Michael Lock <thelockymichael@gmail.com>
2021-04-05 22:05:53 +0300

    fixed package.json and App.test.js for Travis CI part 02

--- App.test.js
+++ App.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 import TestComponent from './TestComponent'
 
 describe('<TestComponent />', () => {
-  it('has 1 child', () => {
+  it('should have 1 child', () => {
     const tree = renderer.create(<TestComponent />).toJSON()
     expect(tree.children.length).toBe(1)
   })
