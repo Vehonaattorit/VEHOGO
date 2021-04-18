a424c6e1317b71bf5eba208ea1472b26bd83474a
Michael Lock <thelockymichael@gmail.com>
2021-04-18 16:22:04 +0300

    modified address view to hide submit button when searching for address, Next Stop LOGIC works

--- views/Address.js
+++ views/Address.js
@@ -1,93 +1,31 @@
-import React, {
-  createContext,
-  useCallback,
-  useReducer,
-  useContext,
-  useState,
-} from 'react'
+import React, {useContext, useState} from 'react'
 import {StyleSheet, Alert, Text, View, KeyboardAvoidingView} from 'react-native'
-import {color} from '../constants/colors'
-import {Input} from 'react-native-elements'
-import {CustomButton} from '../components/CustomButton'
-import {CustomTitle} from '../components/CustomTitle'
-import {AntDesign, FontAwesome} from '@expo/vector-icons'
+
+import {FontAwesome} from '@expo/vector-icons'
 import {updateUser} from '../controllers/userController'
 import {UserContext} from '../contexts'
-import CustomInput from '../components/CustomInput'
 import GooglePlacesInput from '../components/GooglePlaceInput'
 import {googleMapsApiKey} from '../secrets/secrets'
 import firebase from 'firebase/app'
 import CustomButtonIcon from '../components/CustomIconButton'
 
-const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE'
-
-const formReducer = (state, action) => {
-  if (action.type === FORM_INPUT_UPDATE) {
-    const updatedValues = {
-      ...state.inputValues,
-      [action.input]: action.value,
-    }
-    const updatedValidities = {
-      ...state.inputValidities,
-      [action.input]: action.isValid,
-    }
-    let updatedFormIsValid = true
-    for (const key in updatedValidities) {
-      updatedFormIsValid = updatedFormIsValid && updatedValidities[key]
-    }
-    return {
-      formIsValid: updatedFormIsValid,
-      inputValidities: updatedValidities,
-      inputValues: updatedValues,
-    }
-  }
-  return state
-}
-
 export const Address = ({navigation}) => {
   const {user} = useContext(UserContext)
 
   const [address, setAddress] = useState(user.homeAddress || '')
+  const [showSubmit, setShowSubmit] = useState(true)
 
-  const [formState, dispatchFormState] = useReducer(formReducer, {
-    inputValues: {
-      address: user ? user.homeAddress : '',
-      city: user ? user.city : '',
-    },
-    inputValidities: {
-      address: user ? true : false,
-      city: user ? true : false,
-    },
-    formIsValid: user ? true : false,
-  })
-
-  const inputChangeHandler = useCallback(
-    (inputIdentifier, inputValue, inputValidity) => {
-      dispatchFormState({
-        type: FORM_INPUT_UPDATE,
-        value: inputValue,
-        isValid: inputValidity,
-        input: inputIdentifier,
-      })
-    },
-    [dispatchFormState]
-  )
-
-  const submitHandler = useCallback(async () => {
-    if (
-      !formState.formIsValid &&
-      (address.length > 0 || formState.inputValues.city === '')
-    ) {
+  const submitHandler = async () => {
+    if (address.length < 1) {
       Alert.alert(
         'Wrong input!',
-        'Please write an address and city which has at least 1 letter.',
+        'Please write an address which has at least 1 letter.',
         [{text: 'Okay'}]
       )
       return
     }
 
     const data = await getAddressGeoLocation()
-
 
     user.homeAddress = address
     user.city = data.city
@@ -96,11 +34,13 @@ export const Address = ({navigation}) => {
     await updateUser(user)
 
     navigation.navigate('WorkingDays')
-  }, [formState])
+  }
 
   const getAddressGeoLocation = async () => {
     try {
-      console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&language=fi&key=${googleMapsApiKey}`)
+      console.log(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&language=fi&key=${googleMapsApiKey}`
+      )
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&language=fi&key=${googleMapsApiKey}`,
         {
@@ -136,21 +76,23 @@ export const Address = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.icon}>
-        <FontAwesome name="home" size={300} color='#26AAE2' />
+        <FontAwesome name="home" size={300} color="#26AAE2" />
       </View>
       <KeyboardAvoidingView behavior="padding" style={styles.inputContainer}>
         <GooglePlacesInput
+          setShowSubmit={setShowSubmit}
+          showSubmit={showSubmit}
           defaultValue={user.homeAddress}
           setAddress={setAddress}
         />
-        <CustomButtonIcon
-          style={styles.btn}
-          title="Submit"
-          onPress={submitHandler}
-        />
+        {showSubmit && (
+          <CustomButtonIcon
+            style={styles.btn}
+            title="Submit"
+            onPress={submitHandler}
+          />
+        )}
       </KeyboardAvoidingView>
-
-
     </View>
   )
 }
@@ -180,6 +122,5 @@ const styles = StyleSheet.create({
   btn: {
     width: '100%',
   },
-  icon: {
-  },
+  icon: {},
 })
