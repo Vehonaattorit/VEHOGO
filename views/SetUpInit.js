b12dfa40cb1be0a4fde70818d3969537590357ac
jalsson <nilsson.niklas.97@gmail.com>
2021-04-19 08:49:38 +0300

    added google cloud function call

--- views/SetUpInit.js
+++ views/SetUpInit.js
@@ -11,6 +11,7 @@ import {updateWorkTrip} from '../controllers/workTripController'
 import firebase from 'firebase'
 import CustomButtonIcon from '../components/CustomIconButton'
 import {userConverter} from '../models/user'
+import fire from '../firebase/fire'
 
 export const SetUpInit = ({route}) => {
   const {user} = useContext(UserContext)
@@ -117,7 +118,19 @@ export const SetUpInit = ({route}) => {
     if (!user.setupIsCompleted) {
       user.setupIsCompleted = true
       await updateUser(user)
+      let token = await fire.auth().currentUser.getIdTokenResult()
 
+      const response = await fetch(
+        `https://us-central1-veho-go.cloudfunctions.net/getBestRoutes`,
+        {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify({idToken: token}),
+        }
+      )
       if (user.travelPreference === 'driver') await setupWorkTripDocs()
     }
   }
