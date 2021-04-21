74be0e7afd6069f46321b376272efe5572261000
Michael Lock <thelockymichael@gmail.com>
2021-04-21 15:26:33 +0300

    removed undefined setNotification variable

--- App.js
+++ App.js
@@ -50,28 +50,6 @@ export default function App() {
   useEffect(() => {
     subscribeToAuth(authStateChanged)
     loadFonts()
-
-    // // Expo Notificatiosn
-    // registerForPushNotificationsAsync().then((token) => setExpoPushToken(token))
-
-    // This listener is fired whenever a notification is received while the app is foregrounded
-    notificationListener.current = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        setNotification(notification)
-      }
-    )
-
-    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(
-      (response) => {
-        console.log(response)
-      }
-    )
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current)
-      Notifications.removeNotificationSubscription(responseListener.current)
-    }
   }, [])
 
   const authStateChanged = (user) => {
