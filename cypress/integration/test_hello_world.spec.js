06689847fd894dea264a1ed63bda605bdb3ce311
Michael Lock <thelockymichael@gmail.com>
2021-04-28 17:53:44 +0300

    cypress tests should go through: added node script that deletes test@register.com user, 8th attempt

--- cypress/integration/test_hello_world.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Some Test', () => {
-  it('Adds document to test_hello_world collection of Firestore', () => {
-    cy.callFirestore('add', 'test_hello_world', {some: 'value'})
-  })
-
-  it('Login to firebase with test UID', () => {
-    cy.login('HYL1B9teNeheVD0cYspurLtqZhI2')
-  })
-})
